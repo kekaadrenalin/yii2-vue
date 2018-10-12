dc465dfa82dc0d9ade7b632e29556602fffb9cba
kekaadrenalin <kekapor@outlook.com>
2018-10-12 10:01:56 +0300

    add asset bundle

---
+++ src/VueAsset.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace kekaadrenalin\vue;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class VueAsset
+ *
+ * Registers Vue.js
+ *
+ * @link    https://github.com/antkaz/yii2-vue
+ * @author  kekaadrenalin <kekapor@outlook.com>
+ * @license BSD-3-Clause
+ * @package kekaadrenalin\vue
+ */
+class VueAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@bower/vue/dist';
+
+    /**
+     * @inheritdoc
+     */
+    public $js = [
+        YII_ENV_DEV ? 'vue.js' : 'vue.min.js',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
